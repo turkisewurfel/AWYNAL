69ed6a42f2198557ef6193452e2527ecadf6e626
Timothy Beihofner <34278596+Tim3Game@users.noreply.github.com>
2020-03-02 02:26:29 +0100

    Add files via upload

---
+++ lang/en-US.js
@@ -0,0 +1,19 @@
+export default {
+  index: {
+    welcome: 'Welcome to alt:Life Helper !',
+    description: 'This site is an <b>unofficial</b> guide-like tool for the GTA V altV server alt:Life, which is a RPG and Chat based RP server, more you can find <a href="https://altv.life">here</a>.\n' +
+      '            Here you can find different thing that can make your stay on alt:Life simpler, things like:\n' +
+      '            <ul>\n' +
+      '              <li>a Cheat Sheet for RP situations !</li>\n' +
+      '              <li>a Earnings list, where you can learn what gets you how much money!</li>\n' +
+      '              <li>a Paycheck calculator for Trucking and Drug smuggle jobs</li>\n' +
+      '              <li>a list of Vehicles with there prices, tested speeds and more </li>\n' +
+      '            </ul>\n' +
+      '            <h2 class="has-text-centered">Who and why was this site created ?</h2>\n' +
+      '            This site was created by Ťim3Game(#1708) in mid February 2020. Initially, this project was meant to be a project, that was created and could be called as finished even that it has no real end goal.\n' +
+      '            It has the intention of making it easier to find and understand various things and mechanics on the server and discovering ways to simplify some actions and possibly find what is needed at a certain point in time.\n' +
+      '            <h2 class="has-text-centered">Are other languages planned for this site ?</h2>\n' +
+      '            Definitely, just because it is easier for non-native English speakers to read and understand something in their native or very well-understood language.\n' +
+      '            When there will be added other languages than English is not yet clear, but probably sometime by the end of March 2020 they should be already be added'
+  }
+}
